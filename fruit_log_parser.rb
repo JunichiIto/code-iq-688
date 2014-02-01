808bbabff92ce4615a8369bbc930ff80f798e1d3
Junichi Ito <jit@sonicgarden.jp>
2014-02-01 13:57:16 +0900

    Object oriented version.

--- fruit_log_parser.rb
+++ fruit_log_parser.rb
@@ -4,8 +4,25 @@ class FruitLogParser
   end
 
   def self.count_fruits(text)
-    [/\{[^}]+\}/, /\[[^\]]+\]/, /\([^)]+\)/].
-      map{|r| text.scan(r).map{|s| s.scan(/\w+/).size } }.
-      flatten.max
+    %w!() {} []!
+      .map{|brackets| BracketsParser.new(brackets).count_fruits(text) }
+      .flatten.max
+  end
+
+  class BracketsParser
+    def initialize(brackets)
+      @start_char = brackets[0]
+      @end_char = brackets[-1]
+    end
+
+    def count_fruits(text)
+      text.scan(regex).map{|s| s.scan(/\w+/).size }
+    end
+
+    private
+
+    def regex
+      Regexp.new "\\#{@start_char}[^\\#{@end_char}]+\\#{@end_char}"
+    end
   end
 end
