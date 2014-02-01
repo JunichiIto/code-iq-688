cd4ba7c43d56d41ecdcfe6c828eed5b9869aa7b3
Junichi Ito <jit@sonicgarden.jp>
2014-02-01 22:55:41 +0900

    Refactoring.

--- fruit_log_parser.rb
+++ fruit_log_parser.rb
@@ -1,27 +1,26 @@
 class FruitLogParser
+  BRACKET_PAIRS = %w!() {} []!.freeze
+
   def self.parse_log(path)
     File.readlines(path).map{|line| count_fruits(line) }
   end
 
   def self.count_fruits(text)
-    %w!() {} []!.map{|brackets|
-      FruitsCounter.new(brackets).count_max(text)
-    }.max
+    fruits_counters.map{|counter| counter.count_max(text) }.max
+  end
+
+  def self.fruits_counters
+    BRACKET_PAIRS.map{|pair| FruitsCounter.new(pair) }
   end
 
   class FruitsCounter
-    def initialize(brackets)
-      @begin, @end = brackets.chars
+    def initialize(bracket_pair)
+      left, right = bracket_pair.chars
+      @regexp = Regexp.new("\\#{left}[^\\#{right}]+\\#{right}")
     end
 
     def count_max(text)
-      text.scan(regexp).map{|s| s.scan(/\w+/).size }.max || 0
-    end
-
-    private
-
-    def regexp
-      Regexp.new("\\#{@begin}[^\\#{@end}]+\\#{@end}")
+      text.scan(@regexp).map{|s| s.scan(/\w+/).size }.max || 0
     end
   end
 end
