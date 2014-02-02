815619bab4f969f5da46ca36a1b192911fbf15ef
Junichi Ito <jit@sonicgarden.jp>
2014-02-03 05:34:29 +0900

    Enable other parenthesis.

--- fruit_log_parser.rb
+++ fruit_log_parser.rb
@@ -3,23 +3,32 @@ class FruitLogParser
     File.readlines(path).map{|line| count_fruits line }
   end
 
-  #       {[^{}]*}
-  # {[^{]*{[^{}]*}[^}]*}
-  def self.count_fruits(text, count = 0, memo = [])
-    pattern = "\\([^\\)}]*\\)"
-    count.times do
-      pattern = "\\([^\\(]*" + pattern + "[^\\)]*\\)"
-    end
-    r = Regexp.new pattern
-    memo << text.scan(r).map{|s| s.scan(/\w+/).size }
-    if text =~ r
-      return count_fruits(text, count + 1, memo)
-    else
-      return memo.flatten.max
+  def self.count_fruits(text)
+    %w!{} [] ()!.
+      map{|pair| FruitCounter.new(pair).count_fruits(text) }.
+      flatten.max
+  end
+
+  class FruitCounter
+    def initialize(bracket_pair)
+      @left, @right = bracket_pair.chars
     end
 
-    #[/\{[^}]+\}/, /\[[^\]]+\]/, /\([^)]+\)/].
-    #  map{|r| text.scan(r).map{|s| s.scan(/\w+/).size } }.
-    #  flatten.max
+    def count_fruits(text, count = 0, memo = [])
+      # E.g.
+      # count = 0       {[^{}]*}
+      # count = 1 {[^{]*{[^{}]*}[^}]*}
+      pattern = "\\#{@left}[^\\#{@right}]*\\#{@right}"
+      count.times do
+        pattern = "\\#{@left}[^\\#{@left}]*" + pattern + "[^\\#{@right}]*\\#{@right}"
+      end
+      r = Regexp.new pattern
+      memo << text.scan(r).map{|s| s.scan(/\w+/).size }
+      if text =~ r
+        count_fruits(text, count + 1, memo)
+      else
+        memo.flatten.max || 0
+      end
+    end
   end
 end
--- fruit_log_parser_spec.rb
+++ fruit_log_parser_spec.rb
@@ -1,64 +1,66 @@
 require './fruit_log_parser'
 
 describe FruitLogParser do
+  describe '::parse_log' do
+    specify do
+      expect(FruitLogParser.parse_log('./fruits.log')).to eq [3, 6, 3, 5, 4]
+    end
+  end
   describe '::count_fruits' do
     subject { FruitLogParser.count_fruits(text) }
-    describe do
-      let(:text) { '(a)'}
-      it { should eq 1 }
-    end
-    describe do
-      let(:text) { '((a))'}
-      it { should eq 1 }
-    end
-    describe do
-      let(:text) { '(a())'}
-      it { should eq 1 }
-    end
-    describe do
-      let(:text) { '(()a)'}
-      it { should eq 1 }
-    end
-    describe do
-      let(:text) { '(a(b))'}
-      it { should eq 2 }
-    end
-    describe do
-      let(:text) { '((a)b)'}
-      it { should eq 2 }
-    end
-    describe do
-      let(:text) { '(a()b)'}
-      it { should eq 2 }
-    end
-    describe do
-      let(:text) { '(a(b)c)'}
-      it { should eq 3 }
-    end
-    describe do
-      let(:text) { '(a(b)c d)'}
+    describe 'sample' do
+      let(:text) { '{apple strawberry (melon [ apple )}' }
       it { should eq 4 }
     end
-    #describe 'sample' do
-    #  let(:text) { '{apple strawberry (melon [ apple )}' }
-    #  it { should eq 4 }
-    #end
-    #describe '01' do
-    #  let(:text) { '{melon (()melon strawberry)][apple}' }
-    #  it { should eq 4 }
-    #end
-    #describe '02' do
-    #  let(:text) { '[apple apple }{melon](strawberry}(melon]]' }
-    #  it { should eq 3 }
-    #end
-    #describe '03' do
-    #  let(:text) { '({}apple) melon strawberry{melon(apple apple) melon strawberry}' }
-    #  it { should eq 5 }
-    #end
+    describe '01' do
+      let(:text) { '{melon (()melon strawberry)][apple}' }
+      it { should eq 4 }
+    end
+    describe '02' do
+      let(:text) { '[apple apple }{melon](strawberry}(melon]]' }
+      it { should eq 3 }
+    end
+    describe '03' do
+      let(:text) { '({}apple) melon strawberry{melon(apple apple) melon strawberry}' }
+      it { should eq 5 }
+    end
+    context 'same parenthesis nested' do
+      describe do
+        let(:text) { '(a)'}
+        it { should eq 1 }
+      end
+      describe do
+        let(:text) { '((a))'}
+        it { should eq 1 }
+      end
+      describe do
+        let(:text) { '(a())'}
+        it { should eq 1 }
+      end
+      describe do
+        let(:text) { '(()a)'}
+        it { should eq 1 }
+      end
+      describe do
+        let(:text) { '(a(b))'}
+        it { should eq 2 }
+      end
+      describe do
+        let(:text) { '((a)b)'}
+        it { should eq 2 }
+      end
+      describe do
+        let(:text) { '(a()b)'}
+        it { should eq 2 }
+      end
+      describe do
+        let(:text) { '(a(b)c)'}
+        it { should eq 3 }
+      end
+      describe do
+        let(:text) { '(a(b)c d)'}
+        it { should eq 4 }
+      end
+    end
   end
-  #describe '::parse_log' do
-  #  specify do
-  #    expect(FruitLogParser.parse_log('./fruits.log')).to eq [3, 6, 3, 5, 4]
-  #  end
-  #end
 end
