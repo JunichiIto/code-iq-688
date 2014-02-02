8fefcb476f1785a9030a0f68585633ca9f20d5c3
Junichi Ito <jit@sonicgarden.jp>
2014-02-03 05:17:00 +0900

    Change to recursive pattern.

--- fruit_log_parser.rb
+++ fruit_log_parser.rb
@@ -6,31 +6,18 @@ class FruitLogParser
   #       {[^{}]*}
   # {[^{]*{[^{}]*}[^}]*}
   def self.count_fruits(text, count = 0, memo = [])
-    if count == 0
-      r = /\([^\)}]*\)/
-      memo << text.scan(r).map{|s| s.scan(/\w+/).size }
-      if text =~ r
-        return count_fruits(text, count + 1, memo)
-      else
-        return memo.flatten.max
-      end
-    elsif count == 1
-      r = /\([^\(]*\([^\)}]*\)[^\)]*\)/
-      memo << text.scan(r).map{|s| s.scan(/\w+/).size }
-      if text =~ r
-        return count_fruits(text, count + 1, memo)
-      else
-        return memo.flatten.max
-      end
-    elsif count == 2
-      r = /\([^\(]*\([^\(]*\([^\)}]*\)[^\)]*\)[^\)]*\)/
-      memo << text.scan(r).map{|s| s.scan(/\w+/).size }
-      if text =~ r
-        return count_fruits(text, count + 1, memo)
-      else
-        return memo.flatten.max
-      end
+    pattern = "\\([^\\)}]*\\)"
+    count.times do
+      pattern = "\\([^\\(]*" + pattern + "[^\\)]*\\)"
     end
+    r = Regexp.new pattern
+    memo << text.scan(r).map{|s| s.scan(/\w+/).size }
+    if text =~ r
+      return count_fruits(text, count + 1, memo)
+    else
+      return memo.flatten.max
+    end
+
     #[/\{[^}]+\}/, /\[[^\]]+\]/, /\([^)]+\)/].
     #  map{|r| text.scan(r).map{|s| s.scan(/\w+/).size } }.
     #  flatten.max
