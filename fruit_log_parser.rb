5432c6c3306f516cd46b0847d6470a330616bece
Junichi Ito <jit@sonicgarden.jp>
2014-02-03 05:13:34 +0900

    Prototype.

--- fruit_log_parser.rb
+++ fruit_log_parser.rb
@@ -3,9 +3,36 @@ class FruitLogParser
     File.readlines(path).map{|line| count_fruits line }
   end
 
-  def self.count_fruits(text)
-    [/\{[^}]+\}/, /\[[^\]]+\]/, /\([^)]+\)/].
-      map{|r| text.scan(r).map{|s| s.scan(/\w+/).size } }.
-      flatten.max
+  #       {[^{}]*}
+  # {[^{]*{[^{}]*}[^}]*}
+  def self.count_fruits(text, count = 0, memo = [])
+    if count == 0
+      r = /\([^\)}]*\)/
+      memo << text.scan(r).map{|s| s.scan(/\w+/).size }
+      if text =~ r
+        return count_fruits(text, count + 1, memo)
+      else
+        return memo.flatten.max
+      end
+    elsif count == 1
+      r = /\([^\(]*\([^\)}]*\)[^\)]*\)/
+      memo << text.scan(r).map{|s| s.scan(/\w+/).size }
+      if text =~ r
+        return count_fruits(text, count + 1, memo)
+      else
+        return memo.flatten.max
+      end
+    elsif count == 2
+      r = /\([^\(]*\([^\(]*\([^\)}]*\)[^\)]*\)[^\)]*\)/
+      memo << text.scan(r).map{|s| s.scan(/\w+/).size }
+      if text =~ r
+        return count_fruits(text, count + 1, memo)
+      else
+        return memo.flatten.max
+      end
+    end
+    #[/\{[^}]+\}/, /\[[^\]]+\]/, /\([^)]+\)/].
+    #  map{|r| text.scan(r).map{|s| s.scan(/\w+/).size } }.
+    #  flatten.max
   end
 end
