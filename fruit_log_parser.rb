8fc2b8e4f530fee53a3c1727c2907f2e5c60789b
Junichi Ito <jit@sonicgarden.jp>
2014-02-03 20:55:46 +0900

    Use regexp.

--- fruit_log_parser.rb
+++ fruit_log_parser.rb
@@ -1,18 +1,23 @@
 class FruitLogParser
-  BRACKET_PAIRS = %w![] {} ()!.map(&:chars).freeze
+  class RegexCounter
+    GROUP_ENCLOSURE = %w|() {} []|.freeze
+
+    def self.count(str)
+      group_finders = GROUP_ENCLOSURE.map do |enclosure|
+        left, right = enclosure.split(//)
+        Regexp.new("(?<grouped>\\#{left}(?:\\g<grouped>|[^\\#{left}\\#{right}])*\\#{right})")
+        #          /(?<grouped>\((?:\g<grouped>|[^\(\)])*\))/
+      end
+      separator = Regexp.new("[#{Regexp.escape(GROUP_ENCLOSURE.join)} ]")
+      group_finders.map { |finder| str.scan(finder) }.flatten.map { |group| group.split(separator) - [''] }.map(&:size).max
+    end
+  end
 
   def self.parse_log(path)
     File.readlines(path).map{|line| count_fruits line }
   end
 
   def self.count_fruits(text)
-    BRACKET_PAIRS.map{|pairs| count_max(text, *pairs) }.max
-  end
-
-  def self.count_max(text, left, right, stack = [])
-    text.chars.each_with_object([]).each_with_index{|(char, counts), index|
-      stack << index if char == left
-      counts << text[stack.pop..index].scan(/\w+/).count if char == right && stack.any?
-    }.max || 0
+    RegexCounter.count(text)
   end
 end
