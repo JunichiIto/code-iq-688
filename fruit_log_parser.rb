8b259d314c441c883fcac272dfc6726f6fb6f8ed
Junichi Ito <jit@sonicgarden.jp>
2014-02-03 15:34:01 +0900

    Refactoring.

--- fruit_log_parser.rb
+++ fruit_log_parser.rb
@@ -10,20 +10,16 @@ class FruitLogParser
   end
 
   def self.list_counts(text, bracket_pair)
-    start_end_table(text, bracket_pair).map {|s, e|
-      text[s..e].scan(/\w+/).size unless e.nil?
+    range_table(text, bracket_pair).map {|s, e|
+      text[s..e].scan(/\w+/).size
     }
   end
 
-  def self.start_end_table(text, bracket_pair)
-    left, right = bracket_pair.chars
-    text.chars.each_with_object({}).each_with_index do |(char, table), index|
-      if char == left
-        table[index] = nil
-      end
-      if char == right && start_index = table.invert[nil]
-        table[start_index] = index
-      end
-    end
+  def self.range_table(text, bracket_pair)
+    left, right, stack = *bracket_pair.chars, []
+    text.chars.each_with_object({}).each_with_index {|(char, table), index|
+      stack << index if char == left
+      table[stack.pop] = index if char == right && stack.any?
+    }
   end
 end
