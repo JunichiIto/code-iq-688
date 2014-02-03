a03aaf9026be856bf591fc4dd7b7ea646336f854
Junichi Ito <jit@sonicgarden.jp>
2014-02-03 15:59:30 +0900

    Refactoring.

--- fruit_log_parser.rb
+++ fruit_log_parser.rb
@@ -6,10 +6,10 @@ class FruitLogParser
   end
 
   def self.count_fruits(text)
-    TARGET_BRACKETS.map{|pair| max_fruits_count(text, *pair.chars) }.max
+    TARGET_BRACKETS.map{|pair| count_max(text, *pair.chars) }.max
   end
 
-  def self.max_fruits_count(text, left, right, stack: [])
+  def self.count_max(text, left, right, stack: [])
     text.chars.each_with_object([]).each_with_index {|(char, counts), index|
       stack << index if char == left
       counts << text[stack.pop..index].scan(/\w+/).size if char == right && stack.any?
