841dc791f518b1be49a6742e578fd1aa6768f470
Junichi Ito <jit@sonicgarden.jp>
2014-02-03 21:05:51 +0900

    Refactoring

--- fruit_log_parser.rb
+++ fruit_log_parser.rb
@@ -3,13 +3,34 @@ class FruitLogParser
     GROUP_ENCLOSURE = %w|() {} []|.freeze
 
     def self.count(str)
-      group_finders = GROUP_ENCLOSURE.map do |enclosure|
-        left, right = enclosure.split(//)
-        Regexp.new("(?<grouped>\\#{left}(?:\\g<grouped>|[^\\#{left}\\#{right}])*\\#{right})")
-        #          /(?<grouped>\((?:\g<grouped>|[^\(\)])*\))/
-      end
-      separator = Regexp.new("[#{Regexp.escape(GROUP_ENCLOSURE.join)} ]")
-      group_finders.map { |finder| str.scan(finder) }.flatten.map { |group| group.split(separator) - [''] }.map(&:size).max
+      group_finders
+        .map {|finder|str.scan(finder) }.flatten
+        .map {|group| group.split(separator).reject(&:empty?) }
+        .map(&:size).max
+    end
+
+    def self.group_finders
+      GROUP_ENCLOSURE.map {|enclosure|
+        Regexp.new(pattern(enclosure), Regexp::EXTENDED)
+      }
+    end
+
+    # /(?<grouped>\((?:\g<grouped>|[^\(\)])*\))/
+    def self.pattern(enclosure)
+      left, right = enclosure.split(//)
+      <<"PTN"
+      (?<grouped>
+        \\#{left}
+          (?:
+            \\g<grouped> | [^\\#{left}\\#{right}]
+          )*
+        \\#{right}
+      )
+PTN
+    end
+
+    def self.separator
+      Regexp.new("[#{Regexp.escape(GROUP_ENCLOSURE.join)}\s]")
     end
   end
 
