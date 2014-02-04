9747bd21c17f780296c4e7a417a012eeb8c891c6
Junichi Ito <jit@sonicgarden.jp>
2014-02-04 13:34:32 +0900

    Refactoring.

--- fruit_log_parser.rb
+++ fruit_log_parser.rb
@@ -1,12 +1,11 @@
 class FruitLogParser
-  GROUP_ENCLOSURES = %w|() {} []|.map(&:chars).freeze
-
   def self.parse_log(path)
     File.readlines(path).map{|line| count_fruits(line) }
   end
 
   def self.count_fruits(text)
-    GROUP_ENCLOSURES
+    %w|() {} []|
+      .map(&:chars)
       .map{|encl| encl.map{|s| "\\#{s}" } }
       .map{|left, right| /#{left}(?:\g<0>|[^#{left}#{right}])*#{right}/ }
       .map{|r| text.scan(r) }.flatten
