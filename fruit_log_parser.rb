96905fac3f6ad6e9726b033e76af2a4bf5cdd0b7
Junichi Ito <jit@sonicgarden.jp>
2014-02-04 14:10:12 +0900

    Refactoring.

--- fruit_log_parser.rb
+++ fruit_log_parser.rb
@@ -4,9 +4,8 @@ class FruitLogParser
   end
 
   def self.count_fruits(text)
-    %w|() {} []|
-      .map(&:chars)
-      .map{|encl| encl.map{|s| "\\#{s}" } }
+    %|(){}[]|.chars
+      .map{|s| "\\#{s}" }.each_slice(2)
       .map{|left, right| /#{left}(?:\g<0>|[^#{left}#{right}])*#{right}/ }
       .map{|r| text.scan(r) }.flatten
       .map{|s| s.scan(/\w+/).count }
